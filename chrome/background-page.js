5c3a61e0540db881aed848a160ac768493531fd4
Christopher Stoy <Christopher.Stoy@xpo.com>
2018-11-16 13:48:32 -0800

    Working Version!
     - Interrogates the selected element for the first Form control it can find, then uses that as the root for the FormGroup.
     - Currently only supports one form group per component!

--- chrome/background-page.js
+++ chrome/background-page.js
@@ -1,41 +1,55 @@
-(function(chrome) {
+function getControlState(control, controlName = null, recursive = false) {
+  const result = {
+    name: controlName,
+    status: control.status,
+    valid: control.valid,
+    pending: control.pending,
+    enabled: control.enabled,
+    errors: control.errors,
+    dirty: control.dirty,
+    touched: control.touched,
+    value: control.value,
+  };
 
-  function getControlState(control, controlName = null, recursive = false) {
-    const result = {
-      name: controlName,
-      status: control.status,
-      valid: control.valid,
-      pending: control.pending,
-      enabled: control.enabled,
-      errors: control.errors,
-      warnings: control['warnings'],
-      dirty: control.dirty,
-      touched: control.touched,
-      hasError: control.hasError,
-    };
-
-    result.value = control.value;
-
-    if (recursive) {
-      const controls = control.controls;
-      if ( controls ) {
-        result.children =  controls.map( (c, name) => this.getControlState(c, name, recursive));
+  if (recursive) {
+    const controls = control.controls;
+    if ( controls ) {
+      result.children = {};
+      for ( let entry of Object.entries(controls)) {
+        result.children[entry[0]] = this.getControlState(entry[1], entry[0], recursive);
       }
     }
-    return result;
   }
+  return result;
+}
 
-  function getSelectedFormData() {
-    let formData = {};
-  
-    const debugElement = ng.probe($0);
-    if (debugElement) {
-      const component = debugElement.componentInstance;
-      if (component && component.form) {
-        formData = getControlState(component.form, component.controlName, true );
+function getFormGroupFromComponent(component) {
+  if ( component ) {
+    for ( let entry of Object.entries(component)) {
+      const value = entry[1];     
+      if ( typeof value === 'object' ) {
+        // probe a few properties to see if it is a Form Control
+        if ( value.hasOwnProperty('status') && value.hasOwnProperty('_onCollectionChange')) {
+          return entry;
+        }
       }
     }
-    return formData;
   }
+  return null;
+}
 
-})(chrome);
+function getSelectedFormData() {
+  let formData = { empty: 'No Form Selected' };
+
+  const debugElement = ng.probe($0);
+  if (debugElement) {
+    const component = debugElement.componentInstance;
+    const form = getFormGroupFromComponent(component);
+    if (form) {
+      formData = getControlState(form[1], form[0], true );
+    }
+  }
+  return formData;
+}
+
+getSelectedFormData();  
--- chrome/event-page.js
+++ chrome/event-page.js
@@ -24,23 +24,65 @@
 
     // Update the sidebar with selected Form
     updateElementProperties() {
-      const exp = `x = () => { 
-        let formData = {};
-
-        const debugElement = ng.probe($0);
-        if ( debugElement ) {
-          const component = debugElement.componentInstance;
-          if ( component ) {
-            form = component.form || component.formGroup;
-            if ( form ) {
-              formData = form.getRawValue();
+      const exp = `
+      function getControlState(control, controlName = null, recursive = false) {
+        const result = {
+          name: controlName,
+          status: control.status,
+          valid: control.valid,
+          pending: control.pending,
+          enabled: control.enabled,
+          errors: control.errors,
+          dirty: control.dirty,
+          touched: control.touched,
+          value: control.value,
+        };
+      
+        if (recursive) {
+          const controls = control.controls;
+          if ( controls ) {
+            result.children = {};
+            for ( let entry of Object.entries(controls)) {
+              result.children[entry[0]] = this.getControlState(entry[1], entry[0], recursive);
             }
           }
         }
-        return formData;		
-        }; x();`;
+        return result;
+      }
+      
+      function getFormGroupFromComponent(component) {
+        if ( component ) {
+          for ( let entry of Object.entries(component)) {
+            const value = entry[1];     
+            if ( typeof value === 'object' ) {
+              // probe a few properties to see if it is a Form Control
+              if ( value.hasOwnProperty('status') && value.hasOwnProperty('_onCollectionChange')) {
+                return entry;
+              }
+            }
+          }
+        }
+        return null;
+      }
+      
+      function getSelectedFormData() {
+        let formData = { empty: 'No Form Selected' };
+      
+        const debugElement = ng.probe($0);
+        if (debugElement) {
+          const component = debugElement.componentInstance;
+          const form = getFormGroupFromComponent(component);
+          if (form) {
+            formData = getControlState(form[1], form[0], true );
+          }
+        }
+        return formData;
+      }
+      
+      getSelectedFormData();        
+      `;
 
-      this.sidebar.setExpression(exp, 'Form');
+      this.sidebar.setExpression(exp, 'Form Data');
     };
 
   }
